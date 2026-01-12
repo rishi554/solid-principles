b52f64b9de7224426e1e3cc8fcc758661d1e3d72
Rushikesh Borate <brtrushi@gmail.com>
2026-01-12 08:06:49 +0530

    Add SOLID principle files

---
+++ index.php
@@ -0,0 +1,9 @@
+<h3> SOLID Principles: </h3>
+
+<ul style="display:list-item">
+    <li><a href="srp.php"> Single Responsibility Principle </a></li>
+    <li><a href="oecmp.php"> Open for extension and closed for modification Principle </li>
+    <li><a href="lsp.php"> Lisko Substitution Principle </li>
+    <li><a href="isp.php"> Interface Segregation Principle </li>
+    <li><a href="dip.php"> Dependancy Inversion Principle </li>
+</ul>
